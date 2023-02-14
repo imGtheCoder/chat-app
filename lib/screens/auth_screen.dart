567768b3d6c9acfba265635b7c74206ed7c44f51
imGtheCoder <stiopimanol34@gmail.com>
2023-02-14 14:20:13 +0200

    creating users using Firebase auth sdk

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,19 +1,56 @@
 import 'package:chat_app_v2/widgets/auth/auth_form.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
 
 class AuthScreen extends StatefulWidget {
-  const AuthScreen({Key key}) : super(key: key);
+  //const AuthScreen({Key key}) : super(key: key);
 
   @override
   State<AuthScreen> createState() => _AuthScreenState();
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  final _auth = FirebaseAuth.instance;
+
+  void _submitAuthForm(
+    String email,
+    String password,
+    String username,
+    bool isLogin,
+    BuildContext ctx,
+  ) async {
+    AuthResult authResult;
+
+    try {
+      if (isLogin) {
+        authResult = await _auth.signInWithEmailAndPassword(
+            email: email, password: password);
+      } else {
+        authResult = await _auth.createUserWithEmailAndPassword(
+            email: email, password: password);
+      }
+    } on PlatformException catch (error) {
+      var message = 'An error occured, please check your credentials';
+
+      if (error.message != null) {
+        message = error.message;
+      }
+      ScaffoldMessenger.of(ctx).hideCurrentSnackBar();
+      ScaffoldMessenger.of(ctx).showSnackBar(SnackBar(
+        content: Text(message),
+        backgroundColor: Theme.of(ctx).errorColor,
+      ));
+    } catch (error) {
+      print(error);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
-      body: AuthForm(),
+      body: AuthForm(_submitAuthForm),
     );
   }
 }
--- lib/widgets/auth/auth_form.dart
+++ lib/widgets/auth/auth_form.dart
@@ -1,13 +1,44 @@
 import 'package:flutter/material.dart';
 
 class AuthForm extends StatefulWidget {
-  const AuthForm({Key key}) : super(key: key);
+  //const AuthForm({Key key}) : super(key: key);
+  final void Function(
+    String email,
+    String password,
+    String username,
+    bool isLogin,
+    BuildContext ctx,
+  ) submitFn;
+
+  AuthForm(this.submitFn);
 
   @override
   State<AuthForm> createState() => _AuthFormState();
 }
 
 class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+  var _isLogin = true;
+  var _userEmail = '';
+  var _userName = '';
+  var _userPassword = '';
+
+  void _trySubmit() {
+    final isValid = _formKey.currentState
+        .validate(); // triggers all validators and returns a boolean
+    FocusScope.of(context).unfocus();
+    if (isValid) {
+      _formKey.currentState.save(); // triggers the onSaved prop
+      widget.submitFn(
+        _userEmail.trim(),
+        _userPassword.trim(),
+        _userName.trim(),
+        _isLogin,
+        context,
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -17,25 +48,67 @@ class _AuthFormState extends State<AuthForm> {
         child: Padding(
           padding: EdgeInsets.all(16),
           child: Form(
+            key: _formKey,
             child: Column(mainAxisSize: MainAxisSize.min, children: [
               TextFormField(
+                key: ValueKey('email'),
+                validator: (value) {
+                  if (value.isEmpty || !value.contains('@')) {
+                    return 'Please enter a valid email adress';
+                  }
+                  return null;
+                },
                 keyboardType: TextInputType.emailAddress,
                 decoration: InputDecoration(
                   labelText: 'Email adress',
                 ),
+                onSaved: (newValue) {
+                  _userEmail = newValue;
+                },
               ),
+              if (!_isLogin)
+                TextFormField(
+                  key: ValueKey('username'),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter a valid username';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(labelText: 'Username'),
+                  onSaved: (newValue) {
+                    _userName = newValue;
+                  },
+                ),
               TextFormField(
-                decoration: InputDecoration(labelText: 'Username'),
-              ),
-              TextFormField(
+                key: ValueKey('password'),
+                validator: (value) {
+                  if (value.isEmpty || value.length < 7) {
+                    return 'Password must be at least 7 characters long.';
+                  }
+                  return null;
+                },
                 obscureText: true,
                 decoration: InputDecoration(labelText: 'Password'),
+                onSaved: (newValue) {
+                  _userPassword = newValue;
+                },
               ),
               SizedBox(
                 height: 12,
               ),
-              ElevatedButton(onPressed: () {}, child: Text('Login')),
-              TextButton(onPressed: () {}, child: Text('Create new account')),
+              ElevatedButton(
+                  onPressed: _trySubmit,
+                  child: Text(_isLogin ? 'Login' : 'Signup')),
+              TextButton(
+                  onPressed: () {
+                    setState(() {
+                      _isLogin = !_isLogin;
+                    });
+                  },
+                  child: Text(_isLogin
+                      ? 'Create new account'
+                      : 'I already have an account')),
             ]),
           ),
         ),
